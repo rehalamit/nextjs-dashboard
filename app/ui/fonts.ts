16aa129f6d01ce5062eabafc14f9fcba222b8891
rehalamit <124895819+rehalamit@users.noreply.github.com>
2024-03-14 09:13:25 +0530

    Add files via upload

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+// Inter is a google font, imported from 'next/font/google' module
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight: "400", subsets: ['latin']})
